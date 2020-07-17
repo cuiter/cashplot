affbcd89755106329ab188db992707b51519a5c9
Casper Uiterwijk <contact@cuiter.me>
2020-07-17 11:17:36 +0200

    Add README, LICENSE, dev dependencies and show help in CLI

--- cashplot/__main__.py
+++ cashplot/__main__.py
@@ -1,4 +1,8 @@
 from cashplot.main import main
 import sys
 
+if len(sys.argv) != 3:
+    print(sys.argv[0] + " <path/to/transactions.csv> <path/to/config.json>")
+    sys.exit(1)
+
 main(sys.argv[1], sys.argv[2])
