04db665dfee7b58a4b43850e51cc6e8d7057c6ef
cuiter <casper.uiterwijk@gmail.com>
2020-08-30 13:51:56 +0200

    Add support for Dutch transaction files

--- tests/test_source_ing.py
+++ tests/test_source_ing.py
@@ -14,24 +14,38 @@ def test_load_row():
     assert transaction.account == 'NL00MAIN1234567890'
     assert transaction.counter_account == 'NL01WORK0987654321'
     assert transaction.code == 'OV'
-    assert transaction.direction == 'Credit'
+    assert transaction.direction == True
     assert transaction.amount == Decimal('2000')
     assert transaction.tr_type == 'Transfer'
     assert transaction.description == 'Salary for June 2020'
 
 
 def test_load_ing_transactions():
-    with open("tests/test_transactions.csv", "r") as tfp:
+    # English transactions file
+    with open("tests/data/test_transactions.csv", "r") as tfp:
         transactions_uncategorized = load_ing_transactions(tfp)
         assert transactions_uncategorized == [
             INGTransaction(datetime.date(2020, 6, 28), "Company Inc.", "NL00MAIN1234567890",
-                           "NL01WORK0987654321", "OV", "Credit", Decimal('2000'), 'Transfer', 'Salary for June 2020'),
+                           "NL01WORK0987654321", "OV", True, Decimal('2000'), 'Transfer', 'Salary for June 2020'),
             INGTransaction(datetime.date(2020, 6, 28), 'Company Inc.', 'NL00MAIN1234567890',
-                           'NL01WORK0987654321', 'OV', 'Credit', Decimal('500'), 'Transfer', 'Bonus for June 2020'),
+                           'NL01WORK0987654321', 'OV', True, Decimal('500'), 'Transfer', 'Bonus for June 2020'),
             INGTransaction(datetime.date(2020, 6, 29), 'Mr. G', 'NL00MAIN1234567890', 'NL00MAIN1234567890',
-                           'GT', 'Debit', Decimal('100'), 'Online Banking', 'To Orange Savings Account ABC123456'),
+                           'GT', False, Decimal('100'), 'Online Banking', 'To Orange Savings Account ABC123456'),
             INGTransaction(datetime.date(2020, 7, 1), 'Mr. G', 'NL00MAIN1234567890', 'NL00MAIN1234567890',
-                           'GT', 'Debit', Decimal('200'), 'Online Banking', 'To Orange Savings Account DEF999999')
+                           'GT', False, Decimal('200'), 'Online Banking', 'To Orange Savings Account DEF999999')
+        ]
+    # Dutch transactions file
+    with open("tests/data/test_transactions_nl.csv", "r") as tfp:
+        transactions_uncategorized = load_ing_transactions(tfp)
+        assert transactions_uncategorized == [
+            INGTransaction(datetime.date(2020, 6, 28), "Company Inc.", "NL00MAIN1234567890",
+                           "NL01WORK0987654321", "OV", True, Decimal('2000'), 'Overschrijving', 'Salaris for juni 2020'),
+            INGTransaction(datetime.date(2020, 6, 28), 'Company Inc.', 'NL00MAIN1234567890',
+                           'NL01WORK0987654321', 'OV', True, Decimal('500'), 'Overschrijving', 'Bonus voor juni 2020'),
+            INGTransaction(datetime.date(2020, 6, 29), 'Mr. G', 'NL00MAIN1234567890', 'NL00MAIN1234567890',
+                           'GT', False, Decimal('100'), 'Online bankieren', 'Naar Oranje spaarrekening ABC123456'),
+            INGTransaction(datetime.date(2020, 7, 1), 'Mr. G', 'NL00MAIN1234567890', 'NL00MAIN1234567890',
+                           'GT', False, Decimal('200'), 'Online bankieren', 'Naar Oranje spaarrekening DEF999999')
         ]
 
 
