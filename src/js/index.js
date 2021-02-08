be9554fa53b61fbc5689f8aad8d0f1377e8984db
Casper Uiterwijk <contact@cuiter.me>
2021-02-08 21:54:19 +0100

    Initial rewrite to JS (stub project)

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 15, 13, 17],
+  mode: 'markers',
+  type: 'scatter',
+};
+
+const trace2 = {
+  x: [2, 3, 4, 5],
+  y: [16, 5, 11, 9],
+  mode: 'lines',
+  type: 'scatter',
+};
+
+const trace3 = {
+  x: [1, 2, 3, 4],
+  y: [12, 9, 15, 12],
+  mode: 'lines+markers',
+  type: 'scatter',
+};
+
+const data = [trace1, trace2, trace3];
+
+Plotly.newPlot('tester', data);
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+
+    path: path.resolve(__dirname, './src'),
+  },
+};
