776283bd67f0921d0260359f1ffada049c5750ca
cuiter <casper.uiterwijk@gmail.com>
2021-03-21 11:57:01 +0100

    Add unit tests for utils.js

--- tests/test-utils.js
+++ tests/test-utils.js
@@ -1,7 +1,41 @@
 const utils = require("../src/js/lib/utils");
 
 describe("Utils", function () {
-  it("can interleave two lists", function () {
-    expect(utils.interleaveLists([1, 2], [3, 4])).toEqual([1, 3, 2, 4]);
+  it("can determine whether a date is valid", function () {
+    expect(utils.isValidDate(null)).toEqual(false);
+    expect(utils.isValidDate(5)).toEqual(false);
+    expect(utils.isValidDate({})).toEqual(false);
+    expect(utils.isValidDate(new Date("invalid"))).toEqual(false);
+    expect(utils.isValidDate(new Date("2020-01-01"))).toEqual(true);
+  });
+
+  it("can determine whether a regex is valid", function () {
+    expect(utils.isValidRegex(null)).toEqual(false);
+    expect(utils.isValidRegex(5)).toEqual(false);
+    expect(utils.isValidRegex({})).toEqual(false);
+    expect(utils.isValidRegex("invalid\\")).toEqual(false);
+    expect(utils.isValidRegex("^[Vv]ali.*d$")).toEqual(true);
+  });
+
+  it("can fill an object with values", function () {
+    expect(utils.fillObject(["x", "y"], 0.0)).toEqual({ x: 0.0, y: 0.0 });
+
+    const obj1 = utils.fillObject(["sum", "changes"], []);
+    expect(obj1).toEqual({ sum: [], changes: [] });
+    expect(obj1["sum"] === obj1["changes"]); // References same array.
+
+    // Use deep-cloning.
+    const obj2 = utils.fillObject(["sum", "changes"], [], true);
+    expect(obj2).toEqual({ sum: [], changes: [] });
+    expect(obj2["sum"] !== obj2["changes"]); // References different arrays.
+  });
+
+  it("can interleave two arrays", function () {
+    expect(utils.interleaveArrays([1, "duck"], [3, "goose"])).toEqual([
+      1,
+      3,
+      "duck",
+      "goose",
+    ]);
   });
 });
