2e950cc2c36a51106b7c911962b16f9031ec2ec0
Casper Uiterwijk <contact@cuiter.me>
2021-12-19 23:21:10 +0100

    Show initial loaded transactions and accounts totals from source data

--- src/ui/components/views/data-tabs/sources-tab.vue
+++ src/ui/components/views/data-tabs/sources-tab.vue
@@ -2,20 +2,20 @@
     <div class="sources-tab full-size">
         <div class="sources-summary">
             <div>
-                <span class="sources-summary-stat">4</span>
+                <span class="sources-summary-stat">{{ totalAccounts }}</span>
                 <svg class="sources-summary-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M2 22a8 8 0 1 1 16 0H2zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6zm7.363 2.233A7.505 7.505 0 0 1 22.983 22H20c0-2.61-1-4.986-2.637-6.767zm-2.023-2.276A7.98 7.98 0 0 0 18 7a7.964 7.964 0 0 0-1.015-3.903A5 5 0 0 1 21 8a4.999 4.999 0 0 1-5.66 4.957z"/></svg>
                 <br>
                 <span class="sources-summary-label">Accounts</span>
             </div>
             <div>
-                <span class="sources-summary-stat sources-summary-stat-wide">5561</span>
+                <span class="sources-summary-stat sources-summary-stat-wide">{{ totalTransactions }}</span>
                 <svg class="sources-summary-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M19.375 15.103A8.001 8.001 0 0 0 8.03 5.053l-.992-1.737A9.996 9.996 0 0 1 17 3.34c4.49 2.592 6.21 8.142 4.117 12.77l1.342.774-4.165 2.214-.165-4.714 1.246.719zM4.625 8.897a8.001 8.001 0 0 0 11.345 10.05l.992 1.737A9.996 9.996 0 0 1 7 20.66C2.51 18.068.79 12.518 2.883 7.89L1.54 7.117l4.165-2.214.165 4.714-1.246-.719zm8.79 5.931L10.584 12l-2.828 2.828-1.414-1.414 4.243-4.242L13.414 12l2.829-2.828 1.414 1.414-4.243 4.242z"/></svg>
                 <br>
                 <span class="sources-summary-label">Transacties</span>
             </div>
         </div>
         <div class="source-data-list">
-            <div class="source-data-item" v-for="info of getAllSourceDataInfo">
+            <div class="source-data-item" v-for="info of allSourceDataInfo">
                 <span class="source-data-name">{{ info.name }}</span>
                 <!--NOTE: If the viewport gets really small, change the date format-->
                 <span class="source-data-date">{{ getInfoDisplayDate(info) }}</span>
@@ -26,8 +26,7 @@
                 <svg class="source-data-transactions-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M19.375 15.103A8.001 8.001 0 0 0 8.03 5.053l-.992-1.737A9.996 9.996 0 0 1 17 3.34c4.49 2.592 6.21 8.142 4.117 12.77l1.342.774-4.165 2.214-.165-4.714 1.246.719zM4.625 8.897a8.001 8.001 0 0 0 11.345 10.05l.992 1.737A9.996 9.996 0 0 1 7 20.66C2.51 18.068.79 12.518 2.883 7.89L1.54 7.117l4.165-2.214.165 4.714-1.246-.719zm8.79 5.931L10.584 12l-2.828 2.828-1.414-1.414 4.243-4.242L13.414 12l2.829-2.828 1.414 1.414-4.243 4.242z"/></svg>
                 <svg class="source-data-remove-button clickable float-right" @click="onRemoveSourceDataPressed(info.name)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 22" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M4 8h16v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8zm3-3V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v2h5v2H2V5h5zm2-1v1h6V4H9zm0 8v6h2v-6H9zm4 0v6h2v-6h-2z"/></svg>
             </div>
-            <div class="no-source-data-message" v-if="getAllSourceDataInfo.length === 0">
-                <br>
+            <div class="no-source-data-message" v-if="allSourceDataInfo.length === 0">
                 <span>Er zijn nog geen bronnen toegevoegd.</span>
                 <br><br>
                 <ul>
@@ -43,7 +42,7 @@
                     @blur="sourceSelectionOpen = false">
                 <div class="sources-selection"
                      v-bind:class="{ disabled: sourceSelectionOpen === false }">
-                    <span v-for="name of getSourceSelectionNames"
+                    <span v-for="name of sourceSelectionNames"
                           v-on:click="onSourceItemSelected">{{ name }}</span>
                 </div>
                 <span class="float-left select-name">{{ sourceProperties[selectedSource].name }}</span>
@@ -121,7 +120,6 @@ export default {
                     url: "https://bunq.app/"
                 }
             },
-            allSourceDataInfo: [],
             infoDateFormat: longInfoDateFormat,
             resizeEventHandler: null
         }
@@ -136,7 +134,7 @@ export default {
     },
 
     computed: {
-        getSourceSelectionNames() : string[] {
+        sourceSelectionNames() : string[] {
             const selectedSource = (this as any).$data.selectedSource;
             const sourceProperties = (this as any).$data.sourceProperties;
 
@@ -145,8 +143,24 @@ export default {
                 .map(source => sourceProperties[source].name);
         },
 
-        getAllSourceDataInfo() {
+        allSourceDataInfo() {
             return (this as any).$root.$data.state.allSourceDataInfo();
+        },
+
+        // Note: For now, these simply sum up the totals of each source data.
+        //       In the future, it would be useful to remove duplicate
+        //       accounts and transactions from these numbers.
+
+        totalAccounts() {
+            return (this as any).$root.$data.state.allSourceDataInfo()
+                .map((info: SourceDataInfo) => info.nAccounts)
+                .reduce((a: number, b: number) => a + b, 0);
+        },
+
+        totalTransactions() {
+            return (this as any).$root.$data.state.allSourceDataInfo()
+                .map((info: SourceDataInfo) => info.nTransactions)
+                .reduce((a: number, b: number) => a + b, 0);
         }
     },
 
