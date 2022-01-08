caf0455cfc0a561dfc05758ec003d116a739d02d
Casper Uiterwijk <contact@cuiter.me>
2022-01-08 22:42:02 +0100

    Group transactions by date and display in transactions list

--- src/components/ui/components/transaction-list.vue
+++ src/components/ui/components/transaction-list.vue
@@ -1,34 +1,53 @@
 <template>
     <div class="collection-list">
-        <div class="transaction-group">
-            <span class="transaction-date-splitter">12 november 2021</span>
-            <div class="collection-item">
-                <span class="transaction-amount">€17,50</span>
-                <div class="transaction-direction-down"></div>
-                <span class="transaction-title">KPN BV</span>
-                <span class="transaction-description">Simyo Prepaid Simyo</span>
-            </div>
-            <div class="collection-item">
-                <span class="transaction-amount">€107,00</span>
-                <div class="transaction-direction-up"></div>
-                <span class="transaction-title">BELASTINGDIENST</span>
-                <span class="transaction-description">MAAND AUG. NR. 5920294230T2984</span>
-            </div>
-        </div>
-        <div class="transaction-group">
-            <span class="transaction-date-splitter">11 november 2021</span>
-            <div class="collection-item">
-                <span class="transaction-amount">€17,50</span>
-                <div class="transaction-direction-down"></div>
-                <span class="transaction-title">KPN BV</span>
-                <span class="transaction-description">Simyo Prepaid Simyo</span>
+        <div class="transaction-group" v-for="item of transactionsByDate">
+            <span class="transaction-date-splitter">{{ item.date }}</span>
+            <div class="collection-item" v-for="transaction of item.transactions">
+                <span class="transaction-amount">€{{ transaction.amount / DECIMAL }}</span>
+                <div class="transaction-direction-down" v-if="transaction.amount < 0"></div>
+                <div class="transaction-direction-up" v-if="transaction.amount >= 0"></div>
+                <span class="transaction-title">{{ transaction.contraAccountName || transaction.contraAccount }}</span>
+                <span class="transaction-description">{{ transaction.description }}</span>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
+import { DECIMAL, SourceTransaction } from '../../../types';
+import * as dayjs from "dayjs";
+
+const transactionDateFormat = "D MMMM YYYY";
+
 export default {
     props: ["transactions"],
+    computed: {
+        DECIMAL: () => DECIMAL,
+        transactionsByDate: function() {
+            var transactionsByDate = {} as any;
+
+            ((this as any).transactions as SourceTransaction[])
+                .map(tr => tr)
+                .reverse()
+                .forEach(transaction => {
+                    var timeStr = transaction.date.getTime().toString();
+
+                    if (!transactionsByDate.hasOwnProperty(timeStr)) {
+                        transactionsByDate[timeStr] = [];
+                    }
+                    transactionsByDate[timeStr].push(transaction);
+                })
+
+            console.log(transactionsByDate);
+
+            return Object.keys(transactionsByDate)
+                .map(timeStr => {
+                    return {
+                        date: dayjs(new Date(Number(timeStr))).format(transactionDateFormat),
+                        transactions: transactionsByDate[timeStr]
+                    }
+                });
+        }
+    }
 };
 </script>
