088731c00b607ce1b2956fc755f35ce603422292
Casper Uiterwijk <contact@cuiter.me>
2022-05-17 10:03:08 +0200

    Add progress bar component

---
+++ src/view/components/progress-bar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="progress-bar full-width margin-top-small">
+        <div
+            class="progress-bar-success-line"
+            v-bind:style="{ width: successPortionPercentage + '%' }"
+        ></div>
+        <div
+            class="progress-bar-overflow-line"
+            v-bind:style="{
+                left: successPortionPercentage + '%',
+                width: overflowPortionPercentage + '%',
+            }"
+        ></div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    props: {
+        percentage: { type: Number, default: () => 0 },
+    },
+    computed: {
+        successPortionPercentage: function () {
+            const percentage = this.$props.percentage;
+            if (percentage <= 100) {
+                return percentage;
+            } else {
+                return (100 / percentage) * 100;
+            }
+        },
+        overflowPortionPercentage: function () {
+            const percentage = this.$props.percentage;
+            if (percentage <= 100) {
+                return 0;
+            } else {
+                return 100 - (100 / percentage) * 100;
+            }
+        },
+    },
+});
+</script>
