62fc49f82e1276184fad40b706f33f044397fe4e
Casper Uiterwijk <contact@cuiter.me>
2022-05-17 12:17:35 +0200

    Show matching transactions and budget used / total in budget window

--- src/view/windows/budget-window.vue
+++ src/view/windows/budget-window.vue
@@ -1,6 +1,43 @@
 <template>
     <div class="full-size tab-contents">
         <progress-bar-component :percentage="budgetPercentageUsed" />
+        <div class="budget-results flex-center">
+            <div class="budget-results-transactions">
+                <span>{{ categoryTransactionsFiltered.length }}</span>
+                <svg
+                    class="budget-results-transactions-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                >
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path
+                        d="M19.375 15.103A8.001 8.001 0 0 0 8.03 5.053l-.992-1.737A9.996 9.996 0 0 1 17 3.34c4.49 2.592 6.21 8.142 4.117 12.77l1.342.774-4.165 2.214-.165-4.714 1.246.719zM4.625 8.897a8.001 8.001 0 0 0 11.345 10.05l.992 1.737A9.996 9.996 0 0 1 7 20.66C2.51 18.068.79 12.518 2.883 7.89L1.54 7.117l4.165-2.214.165 4.714-1.246-.719zm8.79 5.931L10.584 12l-2.828 2.828-1.414-1.414 4.243-4.242L13.414 12l2.829-2.828 1.414 1.414-4.243 4.242z"
+                    />
+                </svg>
+            </div>
+            <div class="budget-results-used">
+                <span class="text-right">
+                    €{{ Math.abs(budgetUsed.amount / DECIMAL).toFixed(0) }}
+                </span>
+                /
+                <span>{{ Math.abs(budgetAllowed / DECIMAL).toFixed(0) }}</span>
+            </div>
+            <span
+                v-if="budgetUsed.type === 'expenses'"
+                class="budget-results-used-comment"
+            >
+                Uitgegeven deze periode
+            </span>
+            <span
+                v-if="budgetUsed.type === 'income'"
+                class="budget-results-used-comment"
+            >
+                Ontvangen deze periode
+            </span>
+        </div>
+
         <period-selection-component
             v-if="categoryTransactions.length !== 0"
             v-model="filterPeriod"
@@ -17,6 +54,7 @@
 <script lang="ts">
 import Vue from "vue";
 import { Period, PeriodsPerYear, PeriodType } from "../../model/period";
+import { DECIMAL } from "../../model/entities";
 
 const nonExistentCategoryName =
     "non-existent-category-" + new Date().getTime().toString();
@@ -31,6 +69,7 @@ export default Vue.extend({
         };
     },
     computed: {
+        DECIMAL: () => DECIMAL,
         categorySearchQuery: function () {
             const categoryName = ((this as any).openedWindowEntry ?? {})
                 .categoryName;
@@ -58,7 +97,7 @@ export default Vue.extend({
                 (this as any).categoryFilteredSearchQuery,
             );
         },
-        budgetPercentageUsed: function () {
+        budgetUsed: function (): { amount: number; type: string } {
             const categoryName = ((this as any).openedWindowEntry ?? {})
                 .categoryName;
 
@@ -67,22 +106,54 @@ export default Vue.extend({
                     this.$root.$data.cashFlow.calculateCashFlow(
                         (this as any).categoryFilteredSearchQuery,
                     );
-                const biggestFactor =
-                    cashFlow.expenses > cashFlow.income
-                        ? cashFlow.expenses
-                        : cashFlow.income;
 
+                const netCashFlow = cashFlow.income - cashFlow.expenses;
+
+                if (netCashFlow <= 0) {
+                    return {
+                        amount: -netCashFlow,
+                        type: "expenses",
+                    };
+                } else {
+                    return {
+                        amount: netCashFlow,
+                        type: "income",
+                    };
+                }
+            } else {
+                return {
+                    amount: 0,
+                    type: "expenses",
+                };
+            }
+        },
+        budgetAllowed: function () {
+            const categoryName = ((this as any).openedWindowEntry ?? {})
+                .categoryName;
+
+            if (categoryName) {
                 const budget =
                     this.$root.$data.categories.getBudget(categoryName);
 
-                const usedFactor =
-                    ((biggestFactor / budget.amount) *
-                        PeriodsPerYear[
-                            (this as any).filterPeriod.type as PeriodType
-                        ]) /
-                    PeriodsPerYear[budget.periodType as PeriodType];
+                if (budget === null) return 0;
 
-                return usedFactor * 100;
+                return (
+                    (budget.amount *
+                        PeriodsPerYear[budget.periodType as PeriodType]) /
+                    PeriodsPerYear[
+                        (this as any).filterPeriod.type as PeriodType
+                    ]
+                );
+            } else {
+                return 0;
+            }
+        },
+        budgetPercentageUsed: function () {
+            const budgetAllowed: number = this.budgetAllowed as number;
+            const budgetUsed: number = (this.budgetUsed as any)
+                .amount as number;
+            if (budgetAllowed !== 0) {
+                return (budgetUsed / budgetAllowed) * 100;
             } else {
                 return 0;
             }
